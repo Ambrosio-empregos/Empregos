9e89d5a27afc314365b397189877e66f4512d246
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-05 16:52:29 -0300

    adicona nova vaga

--- js/vagas.js
+++ js/vagas.js
@@ -175,6 +175,17 @@ const vagas = [
     email: "",
     banner: "banners/assmanu.jpg"
   },
+  {
+    data: "2026-01-05",
+    titulo: "Lavador de veículos",
+    empresa: "Empresa não identiicada",
+    cidade: "Londrina",
+    descricao: "Oportunidade de Emprego para Lavador de Veículos no Shopping Catuaí. Início imediato com remuneração de R$ 2.200,00!",
+    descricaoModal:"Atividades: Realizar lavagem completa de veículos (externa e interna), aplicação de produtos adequados seguindo normas de segurança, acabamento final e organização do ambiente.",
+    whatsapp: "554391502191",
+    email: "",
+    banner: "banners/lavador.jpg"
+  },
   
   
 ];
