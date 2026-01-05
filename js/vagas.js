bdcdbd5033543fdb1e0a3493366d7abe66d84bdc
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-05 15:38:06 -0300

    alterações no código

--- js/vagas.js
+++ js/vagas.js
@@ -6,7 +6,7 @@ const vagas = [
     cidade: "Londrina",
     descricao: "Se você é comprometido, pontual e busca uma oportunidade em um ambiente organizado e colaborativo, envie seu currículo pelo WHATSAPP",
     descricaoModal:"REQUISITOS: Ensino médio completo; Experiência em caixa/atendimento de restaurante; Capacidade de organização e agilidade; Bom relacionamento em equipe; Será um diferencial ter conhecimento no sistema Sagres.",
-    whatsapp: "5543991319691",
+    whatsapp: "https://wa.me/5543991319691?text=Olá,%20vi%20sua%20vaga%20no%20Ambrósio%20Empregos%20Londrina.%20Quero%20enviar%20meu%20currículo%20para%20a%20vaga%20de...",
     email: "",
     banner: "banners/opcaixa.jpg"
   },
@@ -17,7 +17,7 @@ const vagas = [
     cidade: "Londrina",
     descricao: "Venha fazer parte do nosso time! O Churrasqueiro será responsável pelo preparo das carnes, controle dos pontos e apresentação dos pratos, sempre alinhado aos padrões de excelência da casa. Deverá ter experiência comprovada com churrasqueira a carvao. Vaga para maiores de 18 anos.",
     descricaoModal:"BENEFÍCIOS: Salário Base; Plano de Saúde; Convênio Farmácia; DayOff Aniversário; Refeição na empresa; Vale Transporte;",
-    whatsapp: "5543996610197",
+    whatsapp: "https://wa.me/5543996610197?text=Olá,%20vi%20sua%20vaga%20no%20Ambrósio%20Empregos%20Londrina.%20Quero%20enviar%20meu%20currículo%20para%20a%20vaga%20de...",
     email: "rh@gelobel.com.br",
     banner: "banners/gelchu.jpg"
   },
@@ -28,7 +28,7 @@ const vagas = [
     cidade: "Londrina",
     descricao: "Procuramos alguém para trabalhar como Caixa, cuidando do atendimento no caixa, recebimentos e organização dos valores. Buscamos uma pessoa atenciosa, organizada, com boa comunicação e disponibilidade de horário. Experiência será um diferencial. Vaga para maiores de 18 anos",
     descricaoModal:"BENEFÍCIOS: Salário Base; Plano de Saúde; Convênio Farmácia; DayOff Aniversário; Refeição na empresa; Vale Transporte;",
-    whatsapp: "5543996610197",
+    whatsapp: "https://wa.me/5543996610197?text=Olá,%20vi%20sua%20vaga%20no%20Ambrósio%20Empregos%20Londrina.%20Quero%20enviar%20meu%20currículo%20para%20a%20vaga%20de...",
     email: "rh@gelobel.com.br",
     banner: "banners/gelcaixa.jpg"
   },
@@ -39,7 +39,7 @@ const vagas = [
     cidade: "Londrina",
     descricao: "Procuramos Garçom Garçonete com experiência comprovada em cozinha japonesa, para atuar no atendimento ao cliente, apresentação do cardápio e serviço de mesas, seguindo os padrões da casa. Vagas para maiores de 18 anos.",
     descricaoModal:"BENEFÍCIOS: Salário Base; Plano de Saúde; Convênio Farmácia; DayOff Aniversário; Refeição na empresa; Vale Transporte;",
-    whatsapp: "5543996610197",
+    whatsapp: "https://wa.me/5543996610197?text=Olá,%20vi%20sua%20vaga%20no%20Ambrósio%20Empregos%20Londrina.%20Quero%20enviar%20meu%20currículo%20para%20a%20vaga%20de...",
     email: "rh@gelobel.com.br",
     banner: "banners/belmar.jpg"
   },
@@ -50,7 +50,7 @@ const vagas = [
     cidade: "Rolandia",
     descricao: "Procuramos Atendente Comprometido(a), organizado(a) e com vontade de aprender, para atuar no apoio à equipe.",
     descricaoModal:"REQUISITOS: Ensino Médio completo; Experiência na área; Informática básica; Experiência com prospecção; experiência no uso de CRM.",
-    whatsapp: "5543991349146",
+    whatsapp: "https://wa.me/5543991349146?text=Olá,%20vi%20sua%20vaga%20no%20Ambrósio%20Empregos%20Londrina.%20Quero%20enviar%20meu%20currículo%20para%20a%20vaga%20de...",
     email: "",
     banner: "banners/telemark.jpg"
   },
@@ -61,7 +61,7 @@ const vagas = [
     cidade: "Londrina",
     descricao: "Vaga para Empacotador com atuação em atendimento ao cliente e organização de embalagens. Oportunidade CLT com diversos benefícios em Londrina.",
     descricaoModal:"Requisitos: agilidade e atenção, bom relacionamento e atendimento ao cliente. Atividades: embalar produtos com cuidado e agilidade, auxiliar clientes no transporte das compras até o veículo quando necessário, organizar sacolas e materiais de embalagem, apoiar a equipe de reposição e atendimento quando preciso. Benefícios: cesta básica, cesta de limpeza, plano de saúde, plano odontológico, convênio com farmácia, convênio com a loja, convênio com faculdade, área de convivência, regime CLT e day off aniversário. Local de trabalho: Rua Rio Grande do Norte, 445 Centro Londrina PR.",
-    whatsapp: "5543991367846",
+    whatsapp: "https://wa.me/5543991367846?text=Olá,%20vi%20sua%20vaga%20no%20Ambrósio%20Empregos%20Londrina.%20Quero%20enviar%20meu%20currículo%20para%20a%20vaga%20de...",
     email: "curriculo@grupomp3.com.br",
     banner: "banners/ativaempac.jpg"
   },
@@ -72,7 +72,7 @@ const vagas = [
     cidade: "Londrina",
     descricao: "Vaga para Atendente de Fast Food no Subway com escala 12x36, salário e benefícios. Oportunidade para mulheres maiores de idade.",
     descricaoModal:"Cargo: Atendente de Fast Food. Requisitos: feminino, maior de idade. Jornada: escala 12x36. Remuneração: salário mais benefícios. Contato: envio de currículo por e-mail ou WhatsApp conforme informado pela empresa.",
-    whatsapp: "5543991391333",
+    whatsapp: "https://wa.me/5543991391333?text=Olá,%20vi%20sua%20vaga%20no%20Ambrósio%20Empregos%20Londrina.%20Quero%20enviar%20meu%20currículo%20para%20a%20vaga%20de...",
     email: "subwaymgalondrina@gmail.com",
     banner: "banners/subway.jpg"
   },
@@ -83,7 +83,7 @@ const vagas = [
     cidade: "Londrina",
     descricao: "Vaga para Garçom com remuneração média de R$ 3.020,00, jornada de terça a domingo e atuação na Gleba Palhano.",
     descricaoModal:"Cargo: Garçom. Remuneração média de R$ 3.020,00. Horário de trabalho das 09h30 às 18h00, de terça a domingo. Requisitos: idade mínima de 35 anos e experiência mínima de 1 ano. Contato para candidatura via WhatsApp. Localidade: Gleba Palhano.",
-    whatsapp: "5511999722800",
+    whatsapp: "https://wa.me/5511999722800?text=Olá,%20vi%20sua%20vaga%20no%20Ambrósio%20Empregos%20Londrina.%20Quero%20enviar%20meu%20currículo%20para%20a%20vaga%20de...",
     email: "",
     banner: "banners/garcom.jpg"
   },
@@ -94,7 +94,7 @@ const vagas = [
     cidade: "Arapongas",
     descricao: "Vaga para Atendente de Loja em Sorveteria - Arapongas (Centro) - Ótima oportunidade para quem busca trabalhar com atendimento ao público!",
     descricaoModal:"Requisitos: Ensino Médio completo com excelente comunicação. Experiência ou facilidade para lidar com o atendimento ao cliente. Proatividade, simpatia e organização.",
-    whatsapp: "55439915997711",
+    whatsapp: "https://wa.me/55439915997711?text=Olá,%20vi%20sua%20vaga%20no%20Ambrósio%20Empregos%20Londrina.%20Quero%20enviar%20meu%20currículo%20para%20a%20vaga%20de...",
     email: "valgas3007@gmail.com",
     banner: "banners/sorveteria.jpg"
   },
@@ -105,7 +105,7 @@ const vagas = [
     cidade: "cambé",
     descricao: "Temos Vagas! Atendente na Ritz Sorvetes em Cambé - Disponibilidade para trabalhar à noite e residir na região.",
     descricaoModal:"Escolaridade: Ensino Médio Completo. Requisitos indispensáveis: Habilidades com preparação de pratos, conhecimento em matemática básica, experiência com atendimento ao público e disponibilidade para o período noturno.",
-    whatsapp: "5543991597711",
+    whatsapp: "https://wa.me/5543991597711?text=Olá,%20vi%20sua%20vaga%20no%20Ambrósio%20Empregos%20Londrina.%20Quero%20enviar%20meu%20currículo%20para%20a%20vaga%20de...",
     email: "",
     banner: "banners/ritz.jpg"
   },
@@ -116,7 +116,7 @@ const vagas = [
     cidade: "Londrina",
     descricao: "Vaga para Vendedora na unidade do Shopping Catuaí Londrina. Venha trabalhar com bem-estar e aromatização de ambientes!",
     descricaoModal:"Vaga para o Shopping Catuaí Londrina. Requisitos: Ter mais de 25 anos, Ensino Médio completo, habilidade com vendas, proatividade e dinamismo",
-    whatsapp: "554333017869",
+    whatsapp: "https://wa.me/554333017869?text=Olá,%20vi%20sua%20vaga%20no%20Ambrósio%20Empregos%20Londrina.%20Quero%20enviar%20meu%20currículo%20para%20a%20vaga%20de...",
     email: "prlojasrank@gmail.com",
     banner: "banners/avatim.jpg"
   },
