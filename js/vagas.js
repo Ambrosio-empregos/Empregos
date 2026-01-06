96e597bdcfea344d79ac3a1b340f851f7b4325a9
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-06 10:45:18 -0300

    adiciona nova vaga

--- js/vagas.js
+++ js/vagas.js
@@ -670,4 +670,15 @@ const vagas = [
     email: "",
     banner: "banners/cea.jpeg"
   },
+  {
+    data: "2026-01-05",
+    titulo: "vendedora",
+    empresa: "Billie",
+    cidade: "Londrina",
+    descricao: "A loja Billie em Londrina está contratando Vendedoras! Oferecemos contratos CLT ou Intermitente para atuação no Centro e em Shoppings, incluindo o Catuaí.",
+    descricaoModal:"Informações sobre a Vaga: Contratação para os regimes CLT ou Intermitente. No contrato Intermitente (específico para o Shopping Catuaí), a jornada é realizada conforme a demanda da empresa e disponibilidade do candidato, com pagamento proporcional e direitos garantidos por lei.",
+    whatsapp: "5543999610289",
+    email: "",
+    banner: "banners/billie.jpeg"
+  },
 ];
