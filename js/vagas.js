7a140d6ecedba35cef4833049084bf4570d288aa
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-06 17:17:03 -0300

    nova vaga

--- js/vagas.js
+++ js/vagas.js
@@ -1319,4 +1319,15 @@ const vagas = [
     email: "recrutamentocamponesaimoveis@gmail.com",
     banner: "banners/campo.jpeg"
   },
+  {
+    data: "2026-01-06",
+    titulo: "Angariador de Imóveis",
+    empresa: "Camponesa imóveis",
+    cidade: "Londrina",
+    descricao: "A Camponesa Imóveis está contratando Angariadores de Imóveis com perfil focado em vendas externas e iniciativa.",
+    descricaoModal:"A vaga busca profissionais com boa comunicação, iniciativa, experiência em vendas externas, vontade de aprender e vocação comercial. O pacote de benefícios inclui salário fixo, plano de saúde e ajuda de custo.",
+    whatsapp: "5543999321975",
+    email: "recrutamentocamponesaimoveis@gmail.com",
+    banner: "banners/anga.jpeg"
+  },
 ];
