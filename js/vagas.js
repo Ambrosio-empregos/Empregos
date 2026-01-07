872b4e6c81ab4921704b8388e07f6e9dadca38ed
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-07 14:23:21 -0300

    nova vaga

--- js/vagas.js
+++ js/vagas.js
@@ -2155,5 +2155,16 @@ const vagas = [
     email: "crocs.qq.mga@gmail.com",
     banner: "banners/crocs.jpeg"
   },
+  {
+    data: "2026-01-07",
+    titulo: "Zeladora",
+    empresa: "Guaporé Pneus",
+    cidade: "Maringá",
+    descricao: "Oportunidade para Zeladora em Maringá-PR, oferecendo ótima remuneração salarial e um pacote completo de benefícios, incluindo planos de saúde e odontológico.",
+    descricaoModal:"Detalhes da Oportunidade: A Guaporé Pneus oferece ótima remuneração salarial para a função de Zeladora. Benefícios: Plano de saúde; vale alimentação; Total Pass; plano odontológico; vale transporte. Localização: Maringá-PR.",
+    whatsapp: "554330321911",
+    email: "rh@guaporepneus.com.br",
+    banner: "banners/gua.jpeg"
+  },
   
 ];
