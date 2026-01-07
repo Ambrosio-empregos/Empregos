038e1b178a0b5f0fc186283ddb8440802c99a69a
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-07 16:34:03 -0300

    alteração

--- js/script.js
+++ js/script.js
@@ -149,3 +149,20 @@ function abrirModalImagem(banner) {
 function fecharModal() {
   document.getElementById("vagaModal").style.display = "none";
 }
+
+const btnTopo = document.getElementById("btnTopo");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    btnTopo.style.display = "block";
+  } else {
+    btnTopo.style.display = "none";
+  }
+});
+
+btnTopo.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
