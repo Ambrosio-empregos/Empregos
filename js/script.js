123b92e9572f2ce82ba941b845e79eb416f78be1
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-07 16:57:27 -0300

    topo

--- js/script.js
+++ js/script.js
@@ -152,17 +152,19 @@ function fecharModal() {
 
 const btnTopo = document.getElementById("btnTopo");
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 300) {
-    btnTopo.style.display = "block";
-  } else {
-    btnTopo.style.display = "none";
-  }
-});
-
-btnTopo.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
+if (btnTopo) {
+  btnTopo.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
   });
-});
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+      btnTopo.style.display = "block";
+    } else {
+      btnTopo.style.display = "none";
+    }
+  });
+}
