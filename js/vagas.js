ae279c55302d72fc9d608c7b408fab9130f39611
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-12 16:52:34 -0300

    nova vaga

--- js/vagas.js
+++ js/vagas.js
@@ -3530,5 +3530,16 @@ const vagas = [
     email: "vagas@agrimensura.com.br",
     banner: "banners/topo.jpeg"
   },
+  {
+    data: "2026-01-12", 
+    titulo: "Professor Tecnologia",
+    empresa: "",
+    cidade: "Maringá",
+    descricao: "Oportunidade para Professor de Tecnologia direcionada a graduados ou graduandos em áreas de TI e Engenharia.",
+    descricaoModal:"Requisitos: Destinado a graduados ou graduandos em Análise e Desenvolvimento de Sistemas, Engenharia de Software, Engenharia da Computação e áreas afins.",
+    whatsapp: "5544991315568",
+    email: "maringa@franquia-wmbarros.com.br",
+    banner: "banners/tecno.jpeg"
+  },
 
 ];
