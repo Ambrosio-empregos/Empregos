2c058354f2717da44d3743ec0a69861c9603017d
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-13 14:26:17 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -4234,5 +4234,16 @@ const vagas = [
     email: "",
     banner: "banners/omega3.jpeg"
   },
+ {
+    data: "2026-01-13",
+    titulo: "Secretária",
+    empresa: "Odonto 7",
+    cidade: "Cambé",
+    descricao: "Oportunidade para Secretária na Odonto7 em Cambé, voltada para profissionais proativos com experiência em atendimento ao cliente e sistemas CRM.",
+    descricaoModal:"Requisitos: Ensino médio completo; experiência em atendimento ao cliente; ter excelentes habilidades de comunicação verbal e escrita; perfil proativo, organizado e focado em resultados; conhecimentos com informática e sistemas de agendamento; experiência com sistema de gestão (CRM). Empresa: Odonto7 Cambé.",
+    whatsapp: "5543988726810",
+    email: "",
+    banner: "banners/odo7.jpeg"
+  },
 
 ];
