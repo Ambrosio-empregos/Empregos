87bd6f120389e2f1976563ba0c22f656f44e3716
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-14 09:13:38 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -4498,6 +4498,17 @@ const vagas = [
     email: "catuailondrina@bibi.com.br",
     banner: "banners/bibi.jpeg"
   },
+  {
+    data: "2026-01-14", 
+    titulo: "Vendedor Externo",
+    empresa: "Moto Car",
+    cidade: "Maringá",
+    descricao: "Oportunidade para Vendedor Externo em Maringá/PR na maior rede Honda do Paraná, com entrevistas presenciais nos dias 14 e 15 de janeiro de 2026.",
+    descricaoModal:"Detalhes da Vaga: Processo seletivo para início em 2026; as entrevistas presenciais ocorrerão exclusivamente nos dias 14 e 15 de janeiro. Empresa: Honda Motocar. Localização: Maringá/PR.",
+    whatsapp: "554135153610",
+    email: "",
+    banner: "banners/car.jpeg"
+  },
   
 
 
