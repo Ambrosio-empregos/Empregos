c0ee9d22fae1d3aa35bf15a7abe161cd5391f209
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-14 09:55:32 -0300

    NEW

--- js/vagas.js
+++ js/vagas.js
@@ -4597,6 +4597,17 @@ const vagas = [
     email: "comercial@selatools.com.br",
     banner: "banners/atm.jpeg"
   },
+  {
+    data: "2026-01-14",
+    titulo: "Salgadeiro",
+    empresa: "empresa não identificada",
+    cidade: "Londrina",
+    descricao: "Oportunidade para homens com experiência em panificação ou produção de salgados e pizzas com salário de R$ 3.500,00 + vale-transporte.",
+    descricaoModal:"Vaga para homens. Requisitos: Alguém que tenha experiência em alguma dessas funções (salgadeiro, pizzaiolo ou padeiro). Horário: De segunda a sábado, das 7h às 17h. Salário: R$ 3.500,00 + vale-transporte.",
+    whatsapp: "5543999200044",
+    email: "",
+    banner: "banners/homi.jpeg"
+  },
   
 
 
