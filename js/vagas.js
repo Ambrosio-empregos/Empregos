c77a84cd5eb2b6aac5b823847962ca708f81316c
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-14 16:35:55 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -5092,6 +5092,17 @@ const vagas = [
     email: "fabricadocespio@gmail.com",
     banner: "banners/doceiro.jpeg"
   },
+  {
+    data: "2026-01-14",
+    titulo: "Auxiliar de Limpeza",
+    empresa: "Lidere Service",
+    cidade: "Londrina",
+    descricao: "Oportunidade para Auxiliar de Limpeza na Lidere Service, buscando profissionais organizados e proativos com experiência na área.",
+    descricaoModal:"Requisitos: Vaga para profissional organizado(a) e proativo(a); necessária experiência prévia na área de limpeza; possuir disponibilidade de horários. Detalhes da Oportunidade: Atuação como Auxiliar de Limpeza através da Lidere Service.",
+    whatsapp: "554333570326",
+    email: "",
+    banner: "banners/lidere.jpeg"
+  },
 
 
 ];
