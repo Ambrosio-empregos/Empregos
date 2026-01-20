f4a075eee31ac1a59067d8460a07269b3f0cf928
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-20 16:47:08 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -6720,6 +6720,17 @@ const vagas = [
     email: "villamelti@gmail.com",
     banner: "banners/desejado.jpeg"
   },
+  {
+    data: "2026-01-19",
+    titulo: "Costureiro(a)",
+    empresa: "Incorpast",
+    cidade: "",
+    descricao: "APENAS EMAIL; A Incorpast está contratando Costureira(o)! Oportunidade para profissionais que saibam operar máquinas reta, debrum e Overlock e que gostem de trabalhar em equipe.",
+    descricaoModal:"Funções e Requisitos: Responsável por operar máquina reta, debrum e Overlock, além de auxiliar nas etapas de pré e pós-costura. Buscamos profissionais que gostem de trabalhar em equipe.",
+    whatsapp: "",
+    email: "RH@INCORPAST.COM.BR",
+    banner: "banners/costura.jpeg"
+  },
 
 
   
