9115da440b22d4b51ff30768214089bab46417ca
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-20 17:26:45 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -6896,6 +6896,18 @@ const vagas = [
     email: "",
     banner: "banners/luto.jpeg"
   },
+  {
+    data: "2026-01-19", 
+    titulo: "Supervisor de Expedição e Dobra",
+    empresa: "Principius",
+    cidade: "Maringá",
+    descricao: "Oportunidade para Supervisor de Expedição e Dobra em Mundo Novo - MS, com remuneração de R$ 8.000,00 (PJ), exigindo experiência em liderança no setor têxtil e disponibilidade para mudança.",
+    descricaoModal:"Descrição: Responsável por liderar e supervisionar a equipe de finalização e expedição de produtos de confecção, organizando rotinas, delegando tarefas, acompanhando desempenho e promovendo treinamentos",
+    whatsapp: "5544984012026",
+    email: "contato@principiurh.com.br",
+    banner: "banners/superv.jpeg"
+  },
+
 
 
 
