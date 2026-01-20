d362de69fa14eea7faba017a6e3eeb614bb79f1c
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-20 11:43:47 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -5983,6 +5983,17 @@ const vagas = [
     email: "prlojasrank@gmail.com",
     banner: "banners/avatim.jpg"
   },
+  {
+    data: "2026-01-16",
+    titulo: "Corretores com Creci",
+    empresa: "Camponesa imóveis",
+    cidade: "Londrina",
+    descricao: "Oportunidade para Corretores com CRECI com salário fixo, comissionamento e pacote de benefícios.",
+    descricaoModal:"A vaga busca profissionais dinâmicos e comunicativos, com bom relacionamento interpessoal, conhecimentos em informática, experiência na área comercial e CNH categoria A. A empresa oferece salário fixo, comissionamento, treinamentos e ajuda de custo composta por vale-alimentação e vale-transporte.",
+    whatsapp: "5543999321975",
+    email: "rh@camponesaimoveis.com.br",
+    banner: "banners/corret.jpeg"
+  },
 
 
 ];
