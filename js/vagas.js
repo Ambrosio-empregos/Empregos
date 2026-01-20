3ad620bd2e48c187ddf70dfe3c497a8df73f319c
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-20 16:07:26 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -6533,6 +6533,17 @@ const vagas = [
     email: "",
     banner: "banners/doras.jpeg"
   },
+   {
+    data: "2026-01-19",
+    titulo: "Vendedora",
+    empresa: "Avatim",
+    cidade: "Londrina",
+    descricao: "Vaga para Vendedora na unidade do Shopping Catuaí Londrina. Venha trabalhar com bem-estar e aromatização de ambientes!",
+    descricaoModal:"Vaga para o Shopping Catuaí Londrina. Requisitos: Ter mais de 25 anos, Ensino Médio completo, habilidade com vendas, proatividade e dinamismo",
+    whatsapp: "554333017869",
+    email: "prlojasrank@gmail.com",
+    banner: "banners/avatim.jpg"
+  },
 
 
   
