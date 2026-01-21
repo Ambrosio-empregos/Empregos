403d1fe4ef28800cdc34594d9870eee085bee93c
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-21 17:06:03 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -7512,6 +7512,28 @@ const vagas = [
     email: "contato@principiurh.com.br",
     banner: "banners/atac.jpeg"
   },
+  {
+    data: "2026-01-21", 
+    titulo: "Auxiliar de Cozinha",
+    empresa: "Spedini",
+    cidade: "Londrina",
+    descricao: "Oportunidade para Auxiliar de Cozinha no Spedini Boulevard, exigindo experiência prévia e disponibilidade para trabalhar aos finais de semana.",
+    descricaoModal:"Requisitos: Necessário possuir experiência na função; ter disponibilidade para trabalhar nos finais de semana. Benefícios: Salário fixo; vale-transporte; bônus por resultado da loja; bônus assiduidade; cesta básica.",
+    whatsapp: "5543988122466",
+    email: "",
+    banner: "banners/cozinhaaa.jpeg"
+  },
+  {
+    data: "2026-01-21", 
+    titulo: "Atendente",
+    empresa: "Spedini",
+    cidade: "Londrina",
+    descricao: "Oportunidade para Atendente no Spedini Boulevard, com exigência de experiência e disponibilidade para escalas aos finais de semana.",
+    descricaoModal:"Requisitos: Necessário possuir experiência na função; ter disponibilidade para trabalhar nos finais de semana. Benefícios: Salário fixo; vale-transporte; bônus por resultado da loja; bônus assiduidade; cesta básica.",
+    whatsapp: "5543988122466",
+    email: "",
+    banner: "banners/spend.jpeg"
+  },
 
 
 
