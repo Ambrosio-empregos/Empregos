4d769f4c9340cd542ca80de7d77e37ba22931bd5
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-23 17:42:05 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -8745,6 +8745,28 @@ const vagas = [
     email: "",
     banner: "banners/uso.jpg"
   },
+  {
+    data: "2026-01-23",
+    titulo: "Encanador",
+    empresa: "EMS Engenharia",
+    cidade: "Londrina",
+    descricao: "Oportunidade para Encanador na EMS Engenharia com 5 vagas disponíveis, oferecendo salário base da construção civil e diversos benefícios como vale-compras de R$ 980,00.",
+    descricaoModal:"Requisitos e Benefícios: Necessário experiência na função; salário base da construção civil; vale-compras de R$ 980,00; café da manhã de R$ 8,00 por dia; seguro de vida; vale transporte. Vagas: 5 vagas disponíveis. Empresa: EMS Engenharia - Projeto e execução.",
+    whatsapp: "5543991503783",
+    email: "",
+    banner: "banners/fogo.jpeg"
+  },
+  {
+    data: "2026-01-23",
+    titulo: "Angariador de Imóveis",
+    empresa: "Camponesa Imóveis",
+    cidade: "Londrina",
+    descricao: "Oportunidade para Angariador de Imóveis com salário fixo, comissionamento, treinamentos e ajuda de custo para profissionais com CNH A.",
+    descricaoModal:"Oferecemos: Salário fixo, ajuda de custo (vale alimentação e transporte), comissionamento e treinamentos. Requisitos: Profissional dinâmico, comunicativo, bom relacionamento interpessoal, conhecimento em informática, com experiência na área comercial e CNH A.",
+    whatsapp: "5543999321975",
+    email: "recrutamentocamponesaimoveis@gmail.com",
+    banner: "banners/imo.jpeg"
+  },
   
   
 
