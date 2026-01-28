f9f73b8943556e1ac21c3605499b4ab0b14fe2fb
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-28 10:09:19 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -10109,6 +10109,17 @@ const vagas = [
     email: "rh@karilu.com.br",
     banner: "banners/karilu.jpeg"
   },
+  {
+    data: "2026-01-28",
+    titulo: "Setor de Cadastramento",
+    empresa: "Empresa não identificada",
+    cidade: "Londrina",
+    descricao: "Vaga para o Setor de Cadastramento com ganhos médios entre R$ 2.000,00 e R$ 5.000,00, incluindo CLT, comissão e vale transporte.",
+    descricaoModal:"Requisitos e Atividades: Ser maior de 18 anos; possuir habilidade com redes sociais; experiência com vendas é um diferencial; atuar na prospecção de novos clientes com atendimento presencial e on-line. Detalhes: Ganhos médios entre R$ 2.000,00 e R$ 5.000,00; contratação via CLT, comissão e vale transporte; contratação imediata.",
+    whatsapp: "5543998508587",
+    email: "",
+    banner: "banners/cadastr.jpeg"
+  },
   
   
   
