b23b482b3d474a8bc4b08063bca26114dfc3fba8
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-01-30 08:59:57 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -11033,6 +11033,17 @@ const vagas = [
     email: "",
     banner: "banners/case.jpeg"
   },
+  {
+    data: "2026-01-30",
+    titulo: "Mecânico",
+    empresa: "",
+    cidade: "Londrina",
+    descricao: "Oportunidade para Mecânico com especialidade em mecânica geral e suspensão para agendamento imediato de entrevista.",
+    descricaoModal:"Especialidade: Mecânica geral e suspensão. Ações: Agende já uma entrevista.",
+    whatsapp: "554333279037",
+    email: "",
+    banner: "banners/mete.jpeg"
+  },
 
   
   
