357b8e28b85305dbb9b0e3efe88bd05755b4507e
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-02-02 12:07:07 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -12027,12 +12027,12 @@ const vagas = [
     data: "2026-02-02",
     titulo: "Auxiliar de Produção",
     empresa: "Incorpast",
-    cidade: "",
+    cidade: "Londrina",
     descricao: "APENAS EMAIL; A Incorpast está contratando Auxiliar de Produção! Oportunidade para atuar no setor de colagem de artefatos, refile e operação de máquinas. Envie seu currículo por e-mail.",
     descricaoModal:"Atividades e Funções: Atuar no setor de colagem de artefatos de courvin, espuma e papelão. Realizar atividades de refile e operar máquina de dobra.",
     whatsapp: "",
     email: "RH@INCORPAST.COM.BR",
-    banner: "banners/past.jpeg"
+    banner: "banners/pasta.jpeg"
   },
   
 
