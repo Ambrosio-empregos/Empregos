91317a3e16f8ff7f5434a6fb124f94db25a3dbd3
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-02-02 17:56:20 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -12232,6 +12232,17 @@ const vagas = [
     email: "",
     banner: "banners/mensal.jpeg"
   },
+   {
+    data: "2026-02-02", 
+    titulo: "Consultor(a) Telemarketing",
+    empresa: "Negoci.IA",
+    cidade: "Maringá",
+    descricao: "Oportunidade para Consultor (a) de Telemarketing na Negocia Sales Group, buscando profissionais com experiência prévia em vendas e foco em resultados.",
+    descricaoModal:"Requisitos: Experiência prévia em vendas telemarketing; boa comunicação; orientação para resultados; espírito de equipe. Empresa: Negocia Sales Group.",
+    whatsapp: "5544997309246",
+    email: "",
+    banner: "banners/cucu.jpeg"
+  },
   
 
   
