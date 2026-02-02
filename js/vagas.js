d710389a366d597099ce49cb67958a3af927d403
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-02-02 10:10:31 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -11880,6 +11880,17 @@ const vagas = [
     email: "",
     banner: "banners/esta.jpeg"
   },
+  {
+    data: "2026-01-20",
+    titulo: "Consultor de Vendas",
+    empresa: "Innove Line - TIM",
+    cidade: "Londrina",
+    descricao: "Oportunidade para Consultor de Vendas na Inove Line (TIM) em Londrina, oferecendo comissões agressivas, plano de saúde mental e benefícios como férias coletivas e descontos em academias.",
+    descricaoModal:"Requisitos: Experiência em vendas; proatividade; responsabilidade; boa comunicação. Benefícios: Vale refeição; comissões agressivas; vale transporte; plano odontológico; plano de saúde mental; convênio com farmácia; desconto em academias parceiras; programa nutricional; reconhecimento no iver; férias coletivas.",
+    whatsapp: "5543984267289",
+    email: "",
+    banner: "banners/prova.jpeg"
+  },
   
 
   
