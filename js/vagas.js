05692bb72d3718ad20c88cdb45e886a691b2b6dc
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-02-02 12:03:22 -0300

    NEW

--- js/vagas.js
+++ js/vagas.js
@@ -12001,6 +12001,17 @@ const vagas = [
     email: "rh2@guaporepneus.com.br",
     banner: "banners/guaxe.jpeg"
   },
+  {
+    data: "2026-02-02",
+    titulo: "Costureiro(a)",
+    empresa: "Incorpast",
+    cidade: "",
+    descricao: "APENAS EMAIL; A Incorpast está contratando Costureira(o)! Oportunidade para profissionais que saibam operar máquinas reta, debrum e Overlock e que gostem de trabalhar em equipe.",
+    descricaoModal:"Funções e Requisitos: Responsável por operar máquina reta, debrum e Overlock, além de auxiliar nas etapas de pré e pós-costura. Buscamos profissionais que gostem de trabalhar em equipe.",
+    whatsapp: "",
+    email: "RH@INCORPAST.COM.BR",
+    banner: "banners/costura.jpeg"
+  },
   
 
   
