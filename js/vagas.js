1c2e6723db2f347b15286f769121569636ff0f49
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-02-03 17:02:43 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -12507,6 +12507,17 @@ const vagas = [
     email: "",
     banner: "banners/avive.jpeg"
   },
+  {
+    data: "2026-02-03",
+    titulo: "Vendedor",
+    empresa: "Defina Banker",
+    cidade: "Londrina",
+    descricao: "Oportunidade para Vendedor(a) na Defina Banker para as filiais de Ibiporã e Londrina, oferecendo remuneração fixa mais comissões e premiações.",
+    descricaoModal:"Requisitos: Possuir boa comunicação; ser responsável; ter ensino médio completo; ter disponibilidade de horário. Benefícios: Remuneração fixa; comissão; vale-transporte; premiações. Localização: Vagas disponíveis para as filiais de Ibiporã e Londrina.",
+    whatsapp: "5543988471745",
+    email: "",
+    banner: "banners/defina.jpeg"
+  },
   
 
   
