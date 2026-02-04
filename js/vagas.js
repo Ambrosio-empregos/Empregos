2130bacf90f241ca95442868f78f61d17dfd148c
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-02-04 14:56:30 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -12936,6 +12936,17 @@ const vagas = [
     email: "",
     banner: "banners/cia.jpeg"
   },
+   {
+    data: "2026-02-04",
+    titulo: "Vendedora",
+    empresa: "Giramundo Girassol",
+    cidade: "Londrina",
+    descricao: "Oportunidade para Vendedora na Giramundo Girassol Brinquedos em Londrina (conforme código de área 43), exigindo experiência mínima de um ano em carteira e perfil proativo.",
+    descricaoModal:"Pré-requisitos: Experiência em loja de 1 ano em carteira; ser maior de 18 anos; ser comunicativa; que seja proativa; que goste de vendas.",
+    whatsapp: "5543991864915",
+    email: "",
+    banner: "banners/gira.jpeg"
+  },
  
 
   
