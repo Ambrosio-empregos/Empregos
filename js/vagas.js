4235bbb56aa93089a68b106b42c39f721a1ad6be
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-02-06 09:47:46 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -13717,6 +13717,17 @@ const vagas = [
     email: "",
     banner: "banners/dao.jpeg"
   },
+  {
+    data: "2026-02-06", 
+    titulo: "Auxiliar Pleno de Padaria",
+    empresa: "Dom . Rocha",
+    cidade: "Londrina",
+    descricao: "Oportunidade masculina para Auxiliar Pleno de Padaria em Londrina-PR, com jornada de segunda a domingo e benefícios integrais não descontados em folha.",
+    descricaoModal:"Detalhes da Vaga: Vaga masculina para a unidade Dom Rocha em Londrina. Horário: Segunda a sábado, das 11h40 às 20h00, e domingos, das 06h30 às 12h30.",
+    whatsapp: "5543991019002",
+    email: "",
+    banner: "banners/ana.jpeg"
+  },
  
 
 
