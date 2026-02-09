ac7691c055e5dc9d6886b56b2f461f35e87b7f4e
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-02-09 08:49:17 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -14146,6 +14146,17 @@ const vagas = [
     email: "altenburglda@grupoac.nte",
     banner: "banners/burg.jpeg"
   },
+   {
+    data: "2026-02-09", 
+    titulo: "Consultor(A) de Vendas",
+    empresa: "Dudalina",
+    cidade: "Londrina",
+    descricao: "Oportunidade para Consultor(A) de Vendas na Dudalina do Catuaí Shopping Londrina, com média salarial entre R$ 4.000,00 e R$ 5.000,00 e benefícios.",
+    descricaoModal:"Informações da vaga: Atuação como Consultor(A) de Vendas na loja Dudalina do Catuaí Shopping Londrina. com média salarial entre R$ 4.000,00 e R$ 5.000,00. Benefícios: Vale transporte; Vale alimentação; Plano de Saúde.",
+    whatsapp: "",
+    email: "dudalinalondrina@grupoac.net",
+    banner: "banners/dudi.jpeg"
+  },
  
  
 
