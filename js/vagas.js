6fdf2395c7f628d7beb33d4735b197c15d9585c9
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-02-09 09:40:33 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -14245,6 +14245,17 @@ const vagas = [
     email: "financeiro@hotelthomasi.com.br",
     banner: "banners/cama.jpeg"
   },
+   {
+    data: "2026-02-09",
+    titulo: "Atendente",
+    empresa: "Fini",
+    cidade: "Maringá",
+    descricao: "Vaga para Atendente na Fini do Shopping Catuaí Maringá, oferecendo remuneração composta por salário, benefícios e premiações fixas e variáveis.",
+    descricaoModal:"Detalhes da Oportunidade: Atuar como Atendente no Shopping Catuaí Maringá; horário das 13:50h às 22:10h. Benefícios: Salário; Vale Alimentação; Vale Transporte; premiação fixa e metas variáveis. Localização: Maringá-PR.",
+    whatsapp: "5543988425994",
+    email: "",
+    banner: "banners/fini.jpeg"
+  },
  
  
 
