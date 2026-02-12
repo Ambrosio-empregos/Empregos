0796b12f08d084bd0db63243da7d51b468ecc6f0
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-02-12 09:01:00 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -16379,6 +16379,17 @@ const vagas = [
     email: "",
     banner: "banners/biaa.jpeg"
   },
+  {
+    data: "2026-02-12", 
+    titulo: "Auxiliar de Cozinha",
+    empresa: "Belmar",
+    cidade: "Londrina",
+    descricao: "Oportunidade para Auxiliar de Cozinha em Londrina/PR para apoio à equipe, preparo de insumos e manutenção da cozinha.",
+    descricaoModal:"Perfil: Profissional comprometido(a), organizado(a) e com vontade de aprender. Atividades: Atuar no apoio à equipe, preparo de insumos e manutenção da cozinha. Benefícios: Salário base; plano de saúde; convênio farmácia; Day off aniversário; refeição na empresa; vale transporte. Localização: Londrina/PR.",
+    whatsapp: "5543996610197",
+    email: "rh@gelobel.com.br",
+    banner: "banners/belc.jpeg"
+  },
  
 
 
