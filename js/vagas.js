fbca4952828d065e4163338a50990340d455892b
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-02-12 09:03:13 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -16390,6 +16390,17 @@ const vagas = [
     email: "rh@gelobel.com.br",
     banner: "banners/belc.jpeg"
   },
+  {
+    data: "2026-02-12",
+    titulo: "Professor de Musculação",
+    empresa: "Estação Fitness",
+    cidade: "Londrina",
+    descricao: "Oportunidade para Professor de Musculação na Estação Fitness, para contratação imediata de profissional com CREF ativo e boa comunicação.",
+    descricaoModal:"Requisitos: Experiência na função; CREF ativo; capacidade de inspirar alunos; boa comunicação. Benefícios e Ofertas: Treinamentos constantes; salário compatível com a função; contratação imediata. Empresa: Estação Fitness Academia.",
+    whatsapp: "5543984081727",
+    email: "",
+    banner: "banners/maromba.jpeg"
+  },
  
 
 
