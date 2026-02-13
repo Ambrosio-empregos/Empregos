16486db3496f0a1bffa9a1ca21ecf0dcc5bd1451
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-02-13 14:53:43 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -17107,14 +17107,14 @@ const vagas = [
   },
   {
     data: "2026-02-13", 
-    titulo: "Atendente",
+    titulo: "Auxiliar de Laboratório",
     empresa: "Londrifórmulas",
     cidade: "Londrina",
-    descricao: "A Londrifórmulas em Londrina está contratando Atendente, buscando profissionais proativos com ensino médio completo para integrar sua equipe",
-    descricaoModal:"Requisitos: Ser maior de 18 anos; possuir Ensino Médio completo; demonstrar proatividade",
+    descricao: "A Londrifórmulas em Londrina está contratando Auxiliar de Laboratório, buscando profissionais proativos com ensino médio completo para integrar sua equipe técnica",
+    descricaoModal:"equisitos: Ser maior de 18 anos; possuir Ensino Médio completo; demonstrar proatividade",
     whatsapp: "5543991766108",
     email: "",
-    banner: "banners/autorizo.jpeg"
+    banner: "banners/autorizoo.jpeg"
   },
  
 
