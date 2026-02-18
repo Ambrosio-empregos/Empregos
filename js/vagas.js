e440f58175c7aa9c1ce7f1ebf50b36797cbae94c
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-02-18 10:11:38 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -17699,6 +17699,17 @@ const vagas = [
     email: "",
     banner: "banners/ventre.jpeg"
   },
+   {
+    data: "2026-02-18",
+    titulo: "Cozinheira",
+    empresa: "JL Pizzaria e Costelaria",
+    cidade: "Maringá",
+    descricao: "JL Pizzaria e Costelaria contrata Cozinheiras para turnos da manhã e noite em Maringá (Vila Morangueira). Escala 6x1 com salário e benefícios!",
+    descricaoModal:"Turno Manhã (08:00 às 16:00) e Turno Noite (16:00 às 00:00). Requisitos: Experiência em cozinha (desejável como cozinheira), saber fazer frituras, montagem de buffet e organização geral. Para o turno da noite, é necessário veículo próprio ou morar próximo.",
+    whatsapp: "5544999978523",
+    email: "",
+    banner: "banners/maisam.jpeg"
+  },
   
 
  
