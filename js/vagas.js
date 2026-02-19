a4d6a3463a2e00faae13a2f9209b788c5fd660c7
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-02-19 10:48:54 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -18667,6 +18667,17 @@ const vagas = [
     email: "",
     banner: "banners/psico.jpeg"
   },
+  {
+    data: "2026-02-19",
+    titulo: "Operador de Caixa",
+    empresa: "Max Atacadista",
+    cidade: "Londrina",
+    descricao: "Vaga para Operador de Caixa no Max Norte Shopping - Londrina. Faça sua carreira no Grupo Muffato com diversas oportunidades inclusive para PCD e 60+.",
+    descricaoModal:"Benefícios: Assistência de saúde e odontológica, seguro de vida, clube de vantagens com descontos em farmácias, vale refeição, programas de premiação e oportunidade de carreira pela Uniffato.",
+    whatsapp: "5543984220966",
+    email: "",
+    banner: "banners/facu.jpeg"
+  },
 
  
 
