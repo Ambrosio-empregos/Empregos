ac212502af00b64fe7b323906d5c1a2146af7236
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-02-19 10:23:09 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -18568,6 +18568,17 @@ const vagas = [
     email: "",
     banner: "banners/porr.jpeg"
   },
+   {
+    data: "2026-02-19",
+    titulo: "Auxiliar de Limpeza",
+    empresa: "Beta Serv",
+    cidade: "Londrina",
+    descricao: "Temos Vaga! Auxiliar de Limpeza na Beta Serv. Oportunidade para profissionais com experiência mínima na área e disponibilidade de horário.",
+    descricaoModal:"Requisitos: Experiência mínima comprovada na função. Horário: Jornada semanal de 8h com necessidade de disponibilidade para trabalhar também aos fins de semana intercalados.",
+    whatsapp: "5543988256040",
+    email: "",
+    banner: "banners/beta.jpg"
+  },
 
  
 
