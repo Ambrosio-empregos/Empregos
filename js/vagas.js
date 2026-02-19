8447b5b1505a18c39644ef1136d8cff611abc2be
Ambrosio-empregos <luizgustavoambrosio2@gmail.com>
2026-02-19 09:36:31 -0300

    new

--- js/vagas.js
+++ js/vagas.js
@@ -18491,6 +18491,17 @@ const vagas = [
     email: "",
     banner: "banners/ritz.jpg"
   },
+  {
+    data: "2026-02-19",
+    titulo: "Consultora de Vendas",
+    empresa: "Onlauri",
+    cidade: "Balneário Camboriú",
+    descricao: "Oportunidade para Consultora de Vendas na loja Onlauri em Balneário Camboriú, voltada para profissionais que amam moda, atendimento e desejam integrar um time criativo.",
+    descricaoModal:"Perfil: Profissional que ame moda, atendimento ao cliente e queira fazer parte de um time criativo e acolhedor. Localização: Balneário Camboriú. Empresa: Onlauri.",
+    whatsapp: "5534992454552",
+    email: "marketing@onaluri.com.br",
+    banner: "banners/molt.jpeg"
+  }
 
  
 
